bea7d3c26218f12688c9a1395635863a515183a5
TheBunnyMan123 <bunny@tkbunny.net>
2025-12-25 11:28:21 -0600

    steam

--- modules/programs/steam/default.nix
+++ modules/programs/steam/default.nix
@@ -5,6 +5,7 @@
    programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
    };
 
    hardware.steam-hardware.enable = true;

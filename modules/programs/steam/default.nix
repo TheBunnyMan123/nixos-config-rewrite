25346034e73fcfb2c628580daca30efdd2c59099
TheBunnyMan123 <bunny@tkbunny.net>
2026-02-23 22:04:03 -0600

    steam

--- modules/programs/steam/default.nix
+++ modules/programs/steam/default.nix
@@ -1,18 +1,22 @@
 {
-   pkgs,
-   ...
+	pkgs,
+		...
 }: {
-   programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-   };
+	programs.steam = {
+		enable = true;
+		remotePlay.openFirewall = true;
+		localNetworkGameTransfers.openFirewall = true;
+		extraCompatPackages = with pkgs; [
+			proton-ge-bin
+			steam-play-none
+		];
+	};
 
-   hardware.steam-hardware.enable = true;
+	hardware.steam-hardware.enable = true;
 
-   environment.systemPackages = [
-      pkgs.steam
-      pkgs.steamcmd
-   ];
+	environment.systemPackages = [
+		pkgs.steam
+			pkgs.steamcmd
+	];
 }
 

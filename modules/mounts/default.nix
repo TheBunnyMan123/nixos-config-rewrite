4f36c47a41eef626e861fb915e4a66d2ebb94892
TheBunnyMan123 <bunny@tkbunny.net>
2026-03-19 11:58:11 -0500

    niri stuff

--- modules/mounts/default.nix
+++ modules/mounts/default.nix
@@ -3,13 +3,19 @@
 }: {
 	fileSystems = {
 		"/home/bunny/Sync/figura/data/avatars/niko" = rec {
-			device = "/home/bunny/Sync/figura/avatars/niko";
+			device = "/home/bunny/Sync/figura/avatars/others/niko";
 			options = [ "bind" "ro" ];
 			depends = [ device "/home/bunny/Sync/figura/data/avatars" ];
 		};
 
 		"/home/bunny/Sync/figura/data/avatars/Gen6" = rec {
-			device = "/home/bunny/Sync/figura/avatars/Gen6";
+			device = "/home/bunny/Sync/figura/avatars/old/Gen6";
+			options = [ "bind" "ro" ];
+			depends = [ device "/home/bunny/Sync/figura/data/avatars" ];
+		};
+
+		"/home/bunny/Sync/figura/data/avatars/aeliana" = rec {
+			device = "/home/bunny/Sync/figura/avatars/aeliana";
 			options = [ "bind" "ro" ];
 			depends = [ device "/home/bunny/Sync/figura/data/avatars" ];
 		};

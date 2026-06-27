0673e8bece89010e8c6db5baa899fa815bb2f8d8
TheBunnyMan123 <bunny@tkbunny.net>
2026-06-27 12:17:27 -0500

    gamescope

--- modules/programs/steam/default.nix
+++ modules/programs/steam/default.nix
@@ -8,10 +8,18 @@
 		enable = true;
 		remotePlay.openFirewall = true;
 		localNetworkGameTransfers.openFirewall = true;
+		extraPackages = with pkgs; [
+		  gamescope
+		];
 		extraCompatPackages = with pkgs; [
 			proton-ge-bin
 			steam-play-none
 		];
+	};
+
+	programs.gamescope = {
+		enable = true;
+		capSysNice = false;
 	};
 
 	hardware.steam-hardware.enable = true;

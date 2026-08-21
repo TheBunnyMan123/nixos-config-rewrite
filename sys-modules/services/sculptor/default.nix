53b6ad1fa58cc5f6113625101a02ba6374c236d7
TheBunnyMan123 <bunny@tkbunny.net>
2026-08-20 23:19:58 -0500

    RAHHHH

--- sys-modules/services/sculptor/default.nix
+++ sys-modules/services/sculptor/default.nix
@@ -14,28 +14,33 @@
 			StateDirectoryMode = "0755";
 			WorkingDirectory = "/var/lib/sculptor";
 
-			ExecStartPre = ''
+			ExecStartPre = "${pkgs.writeShellScriptBin "copy" ''
 ${pkgs.coreutils}/bin/cp -n ${pkgs.callPackage "${sys-modules}/../packages/server/sculptor.nix" {}}/bin/sculptor /var/lib/sculptor/sculptor
 ${pkgs.coreutils}/bin/cp -n ${./Config.toml} /var/lib/sculptor/Config.toml
-			'';
+			''}/bin/copy";
 			ExecStart = "/var/lib/sculptor/sculptor";
 			Restart = "on-failure";
 
 			AmbientCapabilities = "CAP_NET_BIND_SERVICE";
 		};
-
 	};
 
 	services.cloudflared = {
 		enable = true;
 
-		tunnels = {
-			"d4ae47ee-15b1-4f0c-a2dd-529b8e3e8247" = {
-				credentialsFile = "/etc/cloudflared-creds/creds";
+		tunnels."ffb8a89a-cb75-4348-9cb3-08727056da82" = {
+			credentialsFile = "/etc/cloudflare-creds/tunnel-figura-sculptor.json";
+			default = "http://localhost:6665";
+			ingress = {
+				"figura-sculptor.beangal.dev" = "http://localhost:6665";
 			};
+			originRequest.disableChunkedEncoding = true;
 		};
 	};
 
+	environment.systemPackages = with pkgs; [
+		cloudflared
+	];
 	networking.firewall.allowedTCPPorts = [ 6665 ];
 }
 
